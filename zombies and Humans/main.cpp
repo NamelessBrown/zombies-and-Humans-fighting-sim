2ebda09d34da8e5b45cec0d96af044425cc078d6
Nameless <tyrebrown873@gmail.com>
2020-05-03 14:06:21 -0400

    Add project files.

---
+++ zombies and Humans/main.cpp
@@ -0,0 +1,10 @@
+// zombies and Humans.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
